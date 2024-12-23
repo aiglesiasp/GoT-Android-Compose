6a4a7d56b18f90b7d60399ba43d2a889559188e3
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 19:40:58 +0100

    Tema 1: Modificado servicio para devolver listado de CharacterRemoteResult

--- app/src/main/java/com/ipa/dev/gamethrones/data/ApiService.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/data/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Path
 interface ApiService {
 
     @GET("/api/v2/Characters")
-    suspend fun getCharacters(): CharacterRemoteResult
+    suspend fun getCharacters(): List<CharacterRemoteResult>
 
     @GET("/api/v2/Characters/{id}")
     suspend fun getCharacter(@Path("id") id: Int): CharacterRemoteResult
