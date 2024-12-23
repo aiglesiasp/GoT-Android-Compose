e1026363551481afc214917aee420f89a8412687
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 19:41:19 +0100

    Tema 1: Modificado CharacterModel para quedarme solo con los datos que quiero

--- app/src/main/java/com/ipa/dev/gamethrones/data/CharacterModel.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/data/CharacterModel.kt
@@ -2,24 +2,18 @@ package com.ipa.dev.gamethrones.data
 
 data class CharacterModel (
     val id: Int,
-    val firstName: String,
-    val lastName: String,
     val fullName: String,
     val title: String,
     val family: String,
-    val image: String,
     val imageUrl: String
 )
 
 val listOfCharacters = (1..100).map {
     CharacterModel(
         id = it,
-        firstName = "First Name $it",
-        lastName = "Last Name $it",
         fullName = "Full Name $it",
         title = "Title $it",
         family = "Family $it",
-        image = "Image $it",
         imageUrl = "https://picsum.photos/200/300?id=$it"
     )
 }
