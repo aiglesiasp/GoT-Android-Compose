0ffb71b6e0e888ad0d5b6e2dd6e976be93a278d4
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 12:47:00 +0100

    Tema 0: Agregado CharacterItem y LazyVerticalGrid en la MainActivity para crear la 1a Screen

--- app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
 import com.ipa.dev.gamethrones.ui.theme.GameThronesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +32,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GameThronesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background)
+                {
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(150.dp),
+                        horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        verticalArrangement = Arrangement.spacedBy(4.dp),
+                        modifier = Modifier.padding(horizontal = 4.dp)
+                    ) {
+                        items(listOfCharacters) { character ->
+                            CharacterItem(character = character)
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +51,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GameThronesTheme {
-        Greeting("Android")
+fun CharacterItem(
+    character: CharacterModel,
+) {
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(2/3f)
+                .background(Color.Gray)
+        )
+        Text(
+            text = character.fullName,
+            style = MaterialTheme.typography.bodySmall,
+            maxLines = 1,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
+
+@Preview
+@Composable
+fun CharacterItemPreview() {
+    CharacterItem(listOfCharacters[0])
+}
+
+
