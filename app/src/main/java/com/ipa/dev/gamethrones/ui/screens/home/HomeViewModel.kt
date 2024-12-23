877cd94e8a69f0485db736f1966dc3eb717d8f9d
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 15:31:24 +0100

    Tema 1: Crear UiState en el ViewModel y creamos una variable accesible

--- app/src/main/java/com/ipa/dev/gamethrones/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/ui/screens/home/HomeViewModel.kt
@@ -1,7 +1,31 @@
 package com.ipa.dev.gamethrones.ui.screens.home
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ipa.dev.gamethrones.CharacterModel
+import com.ipa.dev.gamethrones.listOfCharacters
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
+    var uiState by mutableStateOf(UiState())
+        private set
+
+    data class UiState(
+        val isLoading: Boolean = false,
+        val characters: List<CharacterModel> = emptyList()
+    )
+
+    fun onUiReady() {
+        viewModelScope.launch {
+            uiState = UiState(isLoading = true)
+            delay(2000)
+            uiState = UiState(isLoading = false, characters = listOfCharacters)
+
+        }
+    }
 }
