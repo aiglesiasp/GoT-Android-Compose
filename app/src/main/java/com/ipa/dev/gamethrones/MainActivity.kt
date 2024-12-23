a9f5d3c120c8544b2b6a3ca2d3df8e57a0074c0a
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 14:12:35 +0100

    Tema 0: Agregado NavHost para controlar la navegacion

--- app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
@@ -3,6 +3,10 @@ package com.ipa.dev.gamethrones
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ipa.dev.gamethrones.ui.screens.detail.DetailScreen
 import com.ipa.dev.gamethrones.ui.screens.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +15,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            HomeScreen()
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "home") {
+
+                composable("home") {
+                    HomeScreen()
+                }
+                composable("detail") {
+                    DetailScreen()
+                }
+            }
+
         }
     }
 }
