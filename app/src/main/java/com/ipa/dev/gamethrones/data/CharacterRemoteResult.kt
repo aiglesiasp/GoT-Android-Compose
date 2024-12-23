99abc4570a8ecd9aac29544f1fcceb3e58302746
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 19:41:54 +0100

    Tema 1: Creado modelo para la respuesta de getCharacters

--- app/src/main/java/com/ipa/dev/gamethrones/data/CharacterRemoteResult.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/data/CharacterRemoteResult.kt
@@ -1,12 +1,15 @@
 package com.ipa.dev.gamethrones.data
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CharacterRemoteResult (
     val id: Int,
-    val firstName: String,
-    val lastName: String,
-    val fullName: String,
-    val title: String,
-    val family: String,
-    val image: String,
-    val imageUrl: String
+    val firstName: String?,
+    val lastName: String?,
+    val fullName: String?,
+    val family: String?,
+    val title: String?,
+    val image: String?,
+    val imageUrl: String?,
 )
