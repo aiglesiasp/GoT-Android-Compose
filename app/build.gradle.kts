9e878222eb9cdb3de2a6269e25a5099135d8149b
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 18:26:32 +0100

    Tema 1: Agregado dependencia de JSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlinxSerialization)
 }
 
 android {
@@ -64,4 +65,8 @@ dependencies {
 
     //RETROFIT
     implementation(libs.retrofit)
+
+    //JSON
+    implementation(libs.retrofit.converter.kotlinx.serialization)
+    implementation(libs.kotlinx.serialization.json)
 }
