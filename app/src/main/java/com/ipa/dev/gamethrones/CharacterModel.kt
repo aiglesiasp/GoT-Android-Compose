83c6cd11fabb51ccbda9c75a741ba910317d49ac
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 12:46:18 +0100

    Tema 0: Modelo de datos mockeados

--- app/src/main/java/com/ipa/dev/gamethrones/CharacterModel.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/CharacterModel.kt
@@ -10,3 +10,16 @@ data class CharacterModel (
     val image: String,
     val imageUrl: String
 )
+
+val listOfCharacters = (1..100).map {
+    CharacterModel(
+        id = it,
+        firstName = "First Name $it",
+        lastName = "Last Name $it",
+        fullName = "Full Name $it",
+        title = "Title $it",
+        family = "Family $it",
+        image = "Image $it",
+        imageUrl = "https://picsum.photos/200/300?id=$it"
+    )
+}
