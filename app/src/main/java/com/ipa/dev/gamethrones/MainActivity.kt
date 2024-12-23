e3251729be21e4ffba003d1823fc4dfc349ce204
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 12:56:34 +0100

    Tema 0: Modificado CharacterItem visualmente

--- app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
@@ -3,10 +3,7 @@ package com.ipa.dev.gamethrones
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,7 +17,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -64,7 +61,7 @@ fun CharacterItem(
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(2/3f)
-                .background(Color.Gray)
+                .clip(MaterialTheme.shapes.small)
         )
         Text(
             text = character.fullName,
