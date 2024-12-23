724d30eb2305bb889878e08c68279227e9550fb8
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 14:23:12 +0100

    Tema 0: Gestionamos onClick en Navigation para pasarle un id al Detail y navegar hacia esa Screen

--- app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
@@ -3,9 +3,11 @@ package com.ipa.dev.gamethrones
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.ipa.dev.gamethrones.ui.screens.detail.DetailScreen
 import com.ipa.dev.gamethrones.ui.screens.home.HomeScreen
 
@@ -19,9 +21,14 @@ class MainActivity : ComponentActivity() {
             NavHost(navController = navController, startDestination = "home") {
 
                 composable("home") {
-                    HomeScreen()
+                    HomeScreen(onClick = { character ->
+                        navController.navigate("detail/${character.id}")
+                    })
                 }
-                composable("detail") {
+                composable(
+                    route = "detail/{characterId}",
+                    arguments = listOf(navArgument("characterId") { type = NavType.IntType })
+                ) {
                     DetailScreen()
                 }
             }
