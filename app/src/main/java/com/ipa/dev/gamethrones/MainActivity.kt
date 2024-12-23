b576d2b2be133b7627654360baaa38e6733f757b
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-23 14:31:37 +0100

    Tema 0: Extraido componente a Navigation

--- app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/MainActivity.kt
@@ -3,13 +3,7 @@ package com.ipa.dev.gamethrones
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.ipa.dev.gamethrones.ui.screens.detail.DetailScreen
-import com.ipa.dev.gamethrones.ui.screens.home.HomeScreen
+import com.ipa.dev.gamethrones.ui.screens.Navigation
 
 class MainActivity : ComponentActivity() {
 
@@ -17,26 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "home") {
-
-                composable("home") {
-                    HomeScreen(onClick = { character ->
-                        navController.navigate("detail/${character.id}")
-                    })
-                }
-                composable(
-                    route = "detail/{characterId}",
-                    arguments = listOf(navArgument("characterId") { type = NavType.IntType })
-                ) { navBackStackEntry ->
-                    val characterId = navBackStackEntry.arguments?.getInt("characterId")
-                    DetailScreen(
-                        character = listOfCharacters.first { it.id == characterId },
-                        onBackClick = { navController.popBackStack() }
-                    )
-                }
-            }
-
+            Navigation()
         }
     }
 }
---
+++ app/src/main/java/com/ipa/dev/gamethrones/ui/screens/Navigation.kt
@@ -0,0 +1,34 @@
+package com.ipa.dev.gamethrones.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.ipa.dev.gamethrones.listOfCharacters
+import com.ipa.dev.gamethrones.ui.screens.detail.DetailScreen
+import com.ipa.dev.gamethrones.ui.screens.home.HomeScreen
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+
+        composable("home") {
+            HomeScreen(onClick = { character ->
+                navController.navigate("detail/${character.id}")
+            })
+        }
+        composable(
+            route = "detail/{characterId}",
+            arguments = listOf(navArgument("characterId") { type = NavType.IntType })
+        ) { navBackStackEntry ->
+            val characterId = navBackStackEntry.arguments?.getInt("characterId")
+            DetailScreen(
+                character = listOfCharacters.first { it.id == characterId },
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+    }
+}
