0a807b469732bc2fa75e52e9ea2eacd95fa6d33a
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-26 21:32:02 +0100

    Tema 2: Separado DetailScreen en composables mas pequeños

--- app/src/main/java/com/ipa/dev/gamethrones/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/ui/screens/detail/DetailScreen.kt
@@ -1,6 +1,7 @@
 package com.ipa.dev.gamethrones.ui.screens.detail
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -24,10 +25,10 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.ipa.dev.gamethrones.R
+import com.ipa.dev.gamethrones.data.CharacterModel
 import com.ipa.dev.gamethrones.ui.commonViews.LoadingProgressIndicator
 import com.ipa.dev.gamethrones.ui.screens.home.ScreenTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(
     vm: DetailViewModel = viewModel(),
@@ -37,62 +38,78 @@ fun DetailScreen(
 
     ScreenTheme {
         Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(state.character?.fullName ?: "") },
-                    navigationIcon = {
-                        IconButton(onClick = onBackClick) {
-                            Icon(
-                                imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                                contentDescription = stringResource(R.string.back)
-                            )
-                        }
-                    }
-                )
-            }
+            topBar = { DetailTopBar(state.character?.fullName ?: "", onBackClick) }
         ) { padding ->
 
             if (state.isLoading) {
                 LoadingProgressIndicator(modifier = Modifier.padding(padding))
             }
 
-            Column(
-                modifier = Modifier
-                    .padding(padding)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                AsyncImage(
-                    model = state.character?.imageUrl,
-                    contentDescription = state.character?.fullName,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .aspectRatio(16 / 9f)
-                )
-                Text(
-                    text = state.character?.fullName ?: "",
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.headlineLarge
-                )
-                Text(
-                    text = state.character?.title ?: "",
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.headlineMedium
-                )
-                Text(
-                    text = state.character?.family ?: "",
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.headlineMedium
-                )
+            state.character?.let { character ->
+                DetailScreenContent(character = character, modifier = Modifier.padding(padding))
             }
         }
     }
 }
 
-@Preview
+@Composable
+private fun DetailScreenContent(
+    character: CharacterModel,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        AsyncImage(
+            model = character.imageUrl,
+            contentDescription = character.fullName,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(16 / 9f)
+        )
+        Text(
+            text = character.fullName,
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Text(
+            text = character.title,
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Text(
+            text = character.family,
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun DetailTopBar(
+    title: String,
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                    contentDescription = stringResource(R.string.back)
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
 @Composable
 private fun DetailScreen_Preview() {
-    DetailScreen(
-        onBackClick = {}
+    DetailScreenContent(
+        character = CharacterModel(1, "Leonado Di Caprio", "The best", "Targarian", "")
     )
 }
