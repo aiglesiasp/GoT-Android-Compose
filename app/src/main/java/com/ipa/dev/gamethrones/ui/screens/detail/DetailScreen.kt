0841c7fae4d5ee7bde8ca48e5335006afeab715a
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-26 19:37:58 +0100

    Tema 1: Agregado ProgressIndicator en DetailScreen

--- app/src/main/java/com/ipa/dev/gamethrones/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/ui/screens/detail/DetailScreen.kt
@@ -1,13 +1,16 @@
 package com.ipa.dev.gamethrones.ui.screens.detail
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,6 +19,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
@@ -50,6 +54,18 @@ fun DetailScreen(
                 )
             }
         ) { padding ->
+
+            if (state.isLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
             Column(
                 modifier = Modifier
                     .padding(padding)
