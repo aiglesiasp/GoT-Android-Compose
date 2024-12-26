89dc8fd96c1440bb376e0cbd8edcd6421a720a44
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-26 19:19:03 +0100

    Tema 1: Agregada variable State, Repository y peticion en el DetailViewModel

--- app/src/main/java/com/ipa/dev/gamethrones/ui/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/ui/screens/detail/DetailViewModel.kt
@@ -1,6 +1,33 @@
 package com.ipa.dev.gamethrones.ui.screens.detail
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ipa.dev.gamethrones.data.CharacterModel
+import com.ipa.dev.gamethrones.data.CharactersRepository
+import kotlinx.coroutines.launch
 
 class DetailViewModel : ViewModel() {
+
+    private val repository = CharactersRepository()
+
+    var state by mutableStateOf(UiSate())
+        private set
+
+    data class UiSate(
+        val isLoading: Boolean = false,
+        val character: CharacterModel? = null
+    )
+
+    init {
+        viewModelScope.launch {
+            state = UiSate(isLoading = true)
+            state = UiSate(isLoading = false, character = repository.getCharacter(id))
+
+        }
+    }
+
+
 }
