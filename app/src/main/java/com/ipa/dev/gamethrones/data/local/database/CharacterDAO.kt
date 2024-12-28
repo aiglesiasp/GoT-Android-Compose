8964ca75dbb743419ab535a4a3728fdf6b8d4d08
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-28 14:20:38 +0100

    Tema 4: Agregado metodo de insertar en DAO

--- app/src/main/java/com/ipa/dev/gamethrones/data/local/database/CharacterDAO.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/data/local/database/CharacterDAO.kt
@@ -1,6 +1,8 @@
 package com.ipa.dev.gamethrones.data.local.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ipa.dev.gamethrones.data.local.model.CharacterLocalModel
 
@@ -12,5 +14,8 @@ interface CharacterDAO {
 
     @Query("SELECT * FROM CharacterLocalModel WHERE id = :id")
     suspend fun getCharacter(id: Int): CharacterLocalModel
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(characters: List<CharacterLocalModel>)
 
 }
