89e124088f49bee336e105174dc6cef5cb7ba493
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-28 14:17:29 +0100

    Tema 4: Creado modelo de datos para local

---
+++ app/src/main/java/com/ipa/dev/gamethrones/data/local/model/CharacterLocalModel.kt
@@ -0,0 +1,19 @@
+package com.ipa.dev.gamethrones.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Entity
+data class CharacterLocalModel (
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+
+    val firstName: String?,
+    val lastName: String?,
+    val fullName: String?,
+    val family: String?,
+    val title: String?,
+    val image: String?,
+    val imageUrl: String?,
+)
