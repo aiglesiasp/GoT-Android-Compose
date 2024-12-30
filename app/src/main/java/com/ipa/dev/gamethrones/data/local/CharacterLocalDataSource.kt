f406b7ba00a375646b14a53bbb44509d7040ade2
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-30 12:38:40 +0100

    Tema 4: Arreglamos LocalDataSource para transformar elementos

--- app/src/main/java/com/ipa/dev/gamethrones/data/local/CharacterLocalDataSource.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/data/local/CharacterLocalDataSource.kt
@@ -1,5 +1,6 @@
 package com.ipa.dev.gamethrones.data.local
 
+import com.ipa.dev.gamethrones.data.CharacterModel
 import com.ipa.dev.gamethrones.data.local.database.CharacterDAO
 import com.ipa.dev.gamethrones.data.local.model.CharacterLocalModel
 
@@ -7,9 +8,32 @@ class CharacterLocalDataSource(
     private val dao: CharacterDAO
 ) {
 
-    suspend fun getCharacters(): List<CharacterLocalModel> = dao.getCharacters()
+    suspend fun getCharacters(): List<CharacterModel> = dao.getCharacters().map { it.toDomainModel() }
 
-    suspend fun getCharacter(id: Int): CharacterLocalModel = dao.getCharacter(id)
+    suspend fun getCharacter(id: Int): CharacterModel? = dao.getCharacter(id)?.toDomainModel()
 
-    suspend fun insertAll(characters: List<CharacterLocalModel>) = dao.insertAll(characters)
+    suspend fun insertAll(characters: List<CharacterModel>) = dao.insertAll(characters.map { it.toLocalModel() })
+}
+
+private fun CharacterLocalModel.toDomainModel(): CharacterModel {
+    return CharacterModel(
+        id = id,
+        fullName = if (fullName.isNullOrBlank()) "" else fullName,
+        title = if (title.isNullOrBlank()) "" else title,
+        family = if (family.isNullOrBlank()) "" else family,
+        imageUrl = if (imageUrl.isNullOrBlank()) "" else imageUrl
+    )
+}
+
+private fun CharacterModel.toLocalModel(): CharacterLocalModel {
+    return CharacterLocalModel(
+        id = id,
+        firstName = "",
+        lastName = "",
+        fullName = if (fullName.isNullOrBlank()) "" else fullName,
+        title = if (title.isNullOrBlank()) "" else title,
+        family = if (family.isNullOrBlank()) "" else family,
+        image = "",
+        imageUrl = if (imageUrl.isNullOrBlank()) "" else imageUrl
+    )
 }
