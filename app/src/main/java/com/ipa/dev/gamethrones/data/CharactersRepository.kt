cfffffb56ac81d525a262cb2b5ad00e64262b354
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-30 12:39:48 +0100

    Tema 4: Modificado Repository para que acceder tanto al Local como Remote

--- app/src/main/java/com/ipa/dev/gamethrones/data/CharactersRepository.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/data/CharactersRepository.kt
@@ -1,14 +1,28 @@
 package com.ipa.dev.gamethrones.data
 
+import com.ipa.dev.gamethrones.data.local.CharacterLocalDataSource
 import com.ipa.dev.gamethrones.data.remote.CharacterRemoteDataSource
 
 class CharactersRepository(
+    private val localDataSource: CharacterLocalDataSource,
     private val remoteDataSource: CharacterRemoteDataSource
 ) {
 
-    suspend fun getCharacters(): List<CharacterModel> = remoteDataSource.getCharacters()
+    suspend fun getCharacters(): List<CharacterModel> {
+        if (localDataSource.getCharacters().isEmpty()) {
+            val characters = remoteDataSource.getCharacters()
+            localDataSource.insertAll(characters)
+        }
+        return localDataSource.getCharacters()
+    }
 
-    suspend fun getCharacter(id: Int): CharacterModel = remoteDataSource.getCharacter(id)
+    suspend fun getCharacter(id: Int): CharacterModel {
+        if (localDataSource.getCharacter(id) == null) {
+            val character = remoteDataSource.getCharacter(id)
+            localDataSource.insertAll(listOf(character))
+        }
+        return checkNotNull( localDataSource.getCharacter(id) )
+    }
 }
 
 
