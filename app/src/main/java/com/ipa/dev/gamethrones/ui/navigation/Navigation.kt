490082fb06529ea4f142ced546883d317aeb5396
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-30 13:06:08 +0100

    Tema 4: Arreglado Navigation para pasarle el LocalDataSource por constructor

--- app/src/main/java/com/ipa/dev/gamethrones/ui/navigation/Navigation.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/ui/navigation/Navigation.kt
@@ -1,12 +1,16 @@
 package com.ipa.dev.gamethrones.ui.navigation
 
+import android.app.Application
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.ipa.dev.gamethrones.App
 import com.ipa.dev.gamethrones.data.CharactersRepository
+import com.ipa.dev.gamethrones.data.local.CharacterLocalDataSource
 import com.ipa.dev.gamethrones.data.remote.CharacterRemoteDataSource
 import com.ipa.dev.gamethrones.ui.screens.detail.DetailScreen
 import com.ipa.dev.gamethrones.ui.screens.detail.DetailViewModel
@@ -15,9 +19,13 @@ import com.ipa.dev.gamethrones.ui.screens.home.HomeViewModel
 
 @Composable
 fun Navigation() {
+
+    val app = LocalContext.current.applicationContext as App
     val navController = rememberNavController()
+    val dao = app.db.characterDao()
+    val characterLocalDataSource = CharacterLocalDataSource(dao)
     val characterRemoteDataSource = CharacterRemoteDataSource()
-    val charactersRepository = CharactersRepository(remoteDataSource = characterRemoteDataSource)
+    val charactersRepository = CharactersRepository(localDataSource = characterLocalDataSource, remoteDataSource = characterRemoteDataSource)
 
     NavHost(
         navController = navController,
