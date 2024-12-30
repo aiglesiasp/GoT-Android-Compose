555b5452467562ac116eb8d8e958bba5e7172223
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-12-30 13:08:21 +0100

    Tema 4: Agregamos Flow al DAO para tener base de datos reactiva

--- app/src/main/java/com/ipa/dev/gamethrones/data/local/database/CharacterDAO.kt
+++ app/src/main/java/com/ipa/dev/gamethrones/data/local/database/CharacterDAO.kt
@@ -5,15 +5,16 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ipa.dev.gamethrones.data.local.model.CharacterLocalModel
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CharacterDAO {
 
     @Query("SELECT * FROM CharacterLocalModel")
-    suspend fun getCharacters(): List<CharacterLocalModel>
+    fun getCharacters(): Flow<List<CharacterLocalModel>>
 
     @Query("SELECT * FROM CharacterLocalModel WHERE id = :id")
-    suspend fun getCharacter(id: Int): CharacterLocalModel?
+    fun getCharacter(id: Int): Flow<CharacterLocalModel?>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(characters: List<CharacterLocalModel>)
